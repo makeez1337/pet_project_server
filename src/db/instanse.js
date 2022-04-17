1d2a53e33ac3d041be239f94443f11e89ba760ba
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-17 11:38:19 +0300

    connecting to a database

---
+++ src/db/instanse.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("owu", "postgres", null, {
+    host: "localhost",
+    dialect: "postgres"
+});
+
+module.exports = {
+    sequelize
+}
