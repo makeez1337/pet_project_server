3c4c902f654d94bf18ed9d9b429b30ad00f92105
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 15:43:48 +0300

    added errorHandler

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -5,7 +5,12 @@ const { userRouter } = require('./userRouter');
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('*', (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = {
-    apiRouter: router
-}
+  apiRouter: router,
+};
