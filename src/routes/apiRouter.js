e396d8c95b8e7c96f088fcc65ae2957c01e89aa2
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 21:49:53 +0300

    implemented auth router

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
 const { userRouter } = require('./userRouter');
+const { authRouter } = require('./authRouter');
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/auth', authRouter);
 router.use('*', (err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
