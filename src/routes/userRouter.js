54f4edfa4d295b9f14b5ea40e6e13b1ab20452fa
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 16:12:49 +0300

    scattered methods in folders

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,29 +1,11 @@
 const { Router } = require('express');
 
-const { User } = require('../model/User');
-const { Token } = require('../model/Token');
+const { userController } = require('../controllers/userController');
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll();
-
-    res.json(users);
-  } catch (e) {
-    next(e);
-  }
-});
-router.post('/', async (req, res, next) => {
-  try {
-    const { firstName, lastName, email, password, role, age } = req.body;
-
-    const user = await User.create({ firstName, lastName, email, password, role, age });
-    res.json(user);
-  } catch (e) {
-    next(e);
-  }
-});
+router.get('/', userController.findAll);
+router.post('/', userController.createUser);
 
 module.exports = {
   userRouter: router,
