da485d4360d422d3c3739169e5663531573fec4c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 14:44:50 +0300

    created Token model

---
+++ src/model/Token.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const { sequelize } = require('../db/instanse');
+const { User } = require('./User');
+
+const Token = sequelize.define('Token', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  accessToken: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  refreshToken: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+  },
+});
+
+User.hasMany(Token, { as: 'userId', foreignKey: 'userId' });
+
+module.exports = {
+  Token,
+};
