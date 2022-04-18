76dd4e3d5ac8af725e3d6f9f08f7b9ff8a6b03ed
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 15:42:52 +0300

    implemented method to create users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,15 +1,30 @@
 const { Router } = require('express');
 
 const { User } = require('../model/User');
+const { Token } = require('../model/Token');
 
 const router = Router();
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res, next) => {
+  try {
     const users = await User.findAll();
 
     res.json(users);
-})
+  } catch (e) {
+    next(e);
+  }
+});
+router.post('/', async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password, role, age } = req.body;
+
+    const user = await User.create({ firstName, lastName, email, password, role, age });
+    res.json(user);
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = {
-    userRouter: router
-}
+  userRouter: router,
+};
