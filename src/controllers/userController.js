32bc4aac67d7f7b09411df5b44f4ef3ffa7a32cc
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 18:14:56 +0300

    implemented errorHandler

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const { userService } = require('../services/userService');
+const { ErrorHandler } = require('../error/errorHandler');
 
 class UserController {
   async findAllUsers(req, res, next) {
@@ -53,6 +54,8 @@ class UserController {
       if (updateUser[0] === 1) {
         const user = await userService.findUserById(id);
         res.json(user);
+      } else {
+        next(new ErrorHandler('You are sending incorrect fields'));
       }
     } catch (e) {
       next(e);
