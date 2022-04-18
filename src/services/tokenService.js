2791215b11f3a4f67370d586ee8ecf20acb4d1d8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 00:28:37 +0300

    created token service

---
+++ src/services/tokenService.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+class TokenService {
+  generateTokenPair(payload) {
+    const accessToken = jwt.sign(payload, process.env.SECRET_ACCESS_KEY, {
+      expiresIn: process.env.EXPIRES_ACCESS_IN,
+    });
+    const refreshToken = jwt.sign(payload, process.env.SECRET_REFRESH_KEY, {
+      expiresIn: process.env.EXPIRES_REFRESH_IN,
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  verifyToken(token, secretKey) {
+    return jwt.verify(token, secretKey);
+  }
+}
+
+module.exports = {
+  tokenService: new TokenService(),
+};
