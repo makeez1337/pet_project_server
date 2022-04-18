410cf6b64eccc9ac18e0619f7ab3501ee86b28d5
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-18 17:25:19 +0300

    implemented new methods such as deleteById, updateUserById, findUserById

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,22 @@
 const { userService } = require('../services/userService');
 
 class UserController {
-  async findAll(req, res, next) {
+  async findAllUsers(req, res, next) {
     try {
       const users = await userService.findAllUsers();
 
       res.json(users);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async findUserById(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const user = await userService.findUserById(id);
+      res.json(user);
     } catch (e) {
       next(e);
     }
@@ -16,6 +27,33 @@ class UserController {
       const user = await userService.createUser(req.body);
 
       res.json(user);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteById(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      await userService.deleteById(id);
+      res.json(`User with id:${id} was deleted successfully`);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateUserById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { ...fields } = req.body;
+
+      const updateUser = await userService.updateUserById(id, fields);
+
+      if (updateUser[0] === 1) {
+        const user = await userService.findUserById(id);
+        res.json(user);
+      }
     } catch (e) {
       next(e);
     }
