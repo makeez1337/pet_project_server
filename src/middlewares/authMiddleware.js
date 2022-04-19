da1b52626d089be62e51e9f0bc4640b501edeb4a
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 17:10:22 +0300

    created method "checkAccessToken"

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,8 @@
 const { authValidator } = require('../validators/auth/authValidator');
 const { ErrorHandler } = require('../error/errorHandler');
 const { userService } = require('../services/userService');
+const { constants } = require('../constants/constants');
+const { tokenService } = require('../services/tokenService');
 
 class AuthMiddleware {
   isRegistrationValid(req, res, next) {
@@ -47,6 +49,36 @@ class AuthMiddleware {
 
     req.user = user.dataValues;
     next();
+  }
+
+  async checkAccessToken(req, res, next) {
+    try {
+      const accessToken = req.get(constants.headerAuthorization);
+
+      if (!accessToken) {
+        next(new ErrorHandler('Token is not valid', 401));
+        return;
+      }
+
+      const { userEmail } = await tokenService.verifyToken(accessToken);
+
+      const userFromToken = await userService.findUserByParams({ email: userEmail });
+      const tokenPair = await tokenService.findTokenByParams({ accessToken });
+
+      if (!userFromToken) {
+        next(new ErrorHandler('Token is not valid', 401));
+        return;
+      }
+
+      if (!tokenPair) {
+        next(new ErrorHandler('Token is not valid', 401));
+      }
+
+      req.user = userFromToken;
+      next();
+    } catch (e) {
+      next(e);
+    }
   }
 }
 
