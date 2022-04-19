47730515783c2fab0c7cf49d8c30a0eaac75e49b
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 12:04:40 +0300

    created auth middleware and added method isRegistrationValid

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,23 @@
-class AuthMiddleware {}
+const { authValidator } = require('../validators/auth/authValidator');
+const { ErrorHandler } = require('../error/errorHandler');
+
+class AuthMiddleware {
+  isRegistrationValid(req,res,next) {
+    try {
+      const {error, value} = authValidator.registration.validate(req.body);
+
+      if (error) {
+        next(new ErrorHandler(error.details[0].message));
+        return;
+      }
+
+      req.body = value;
+      next();
+    } catch (e) {
+      next(e);
+    }
+  }
+}
 
 module.exports = {
   authMiddleware: new AuthMiddleware(),
