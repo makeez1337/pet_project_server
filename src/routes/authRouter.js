1488fac768db81cf9920f3bc80385356ffec6dbe
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 12:04:08 +0300

    implemented middleware for "registration" route

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const { authController } = require('../controllers/authController');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 
 const router = Router();
 
-router.post('/registration', authController.registration);
+router.post('/registration', authMiddleware.isRegistrationValid, authController.registration);
 
 module.exports = {
   authRouter: router,
