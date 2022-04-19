cb6ecb42ee143021164e1d7b20b98900f4ec957a
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 15:18:10 +0300

    implemented method in controller for "login"

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { userService } = require('../services/userService');
 const { Token } = require('../model/Token');
 const { tokenService } = require('../services/tokenService');
+const { ErrorHandler } = require('../error/errorHandler');
 
 class AuthController {
   async registration(req, res, next) {
@@ -12,6 +13,31 @@ class AuthController {
 
       const userTokenData = await Token.create({ accessToken, refreshToken, userId: id });
       res.json(userTokenData.dataValues);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+      const { password: hashedPassword, id } = req.user;
+      const { email, password } = req.body;
+
+      const isPasswordEqual = await userService.comparePassword(password, hashedPassword);
+
+      if (!isPasswordEqual) {
+        next(new ErrorHandler('Email or password is not valid'));
+        return;
+      }
+
+      const { accessToken, refreshToken } = tokenService.generateTokenPair({ userId: id, userEmail: email });
+      await Token.create({ accessToken, refreshToken, userId: id });
+
+      res.json({
+        accessToken,
+        refreshToken,
+        user: req.user,
+      });
     } catch (e) {
       next(e);
     }
