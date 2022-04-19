d4809617eb358a4fab627e2a77531fed14a47ee8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 15:17:16 +0300

    created "login" endpoint

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -6,6 +6,7 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = Router();
 
 router.post('/registration', authMiddleware.isRegistrationValid, authController.registration);
+router.post('/login', authMiddleware.isLoginValid, authMiddleware.isUserExists, authController.login);
 
 module.exports = {
   authRouter: router,
