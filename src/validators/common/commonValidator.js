95c9984af0fc9fbd852b7586a3ed01839d9280d1
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-19 15:22:36 +0300

    reformatted code

--- src/validators/common/commonValidator.js
+++ src/validators/common/commonValidator.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const { constants } = require('../../constants/constants');
 
 const commonValidator = {
-  emailValidator: Joi.string().pattern(constants.EMAIL_REGEXP).trim().required(),
+  emailValidator: Joi.string().pattern(constants.emailRegexp).trim().required(),
   passwordValidator: Joi.string().alphanum().min(8).trim().required(),
 };
 
