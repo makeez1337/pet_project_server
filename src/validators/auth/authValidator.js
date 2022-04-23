f26a4f96c43d0ff2a43776ec5a21ff7606629726
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-23 13:41:26 +0300

    removed field age

--- src/validators/auth/authValidator.js
+++ src/validators/auth/authValidator.js
@@ -6,7 +6,6 @@ const authValidator = {
   registration: Joi.object().keys({
     firstName: Joi.string().trim().required(),
     lastName: Joi.string().trim().required(),
-    age: Joi.number().integer().greater(1).less(100).required(),
     email: commonValidator.emailValidator,
     password: commonValidator.passwordValidator,
   }),
