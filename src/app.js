bbd0ea55c68d6b0da40ca5a878e3dfeb2dc6adeb
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-29 09:31:49 +0300

    added cookie parser mdlwr

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ require('dotenv').config();
 const { User } = require('./model/User');
 const { apiRouter } = require('./routes/apiRouter');
 const { sequelize } = require('./db/instanse');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
