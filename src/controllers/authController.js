05fccb9fdd983555caf99d31145918166d002f6e
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-29 14:40:27 +0300

    removed redundant service in refresh controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -65,7 +65,6 @@ class AuthController {
 
   async refresh(req, res, next) {
     const { refreshToken: refreshTokenFromCookie } = req.cookies;
-    const { id: tokenId } = await tokenService.findTokenByParams({ refreshToken: refreshTokenFromCookie });
     const { email, id } = req.user;
     const user = new UserDto(req.user);
 
