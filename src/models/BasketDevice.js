06f23a8a75e277fa29d7acbea4aa7ff69f43e4e8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-05 15:25:57 +0300

    created model basketDevice

---
+++ src/models/BasketDevice.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+
+const { sequelize } = require('../db/instanse');
+const { Basket } = require('./Basket');
+const { Phone } = require('./Phone');
+
+const BasketDevice = sequelize.define('basketDevice', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  phoneId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Phone,
+      key: 'id',
+    },
+  },
+  basketId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Basket,
+      key: 'id',
+    },
+  },
+});
+
+Basket.hasMany(BasketDevice, { foreignKey: 'basketId', onDelete: 'CASCADE' });
+BasketDevice.belongsTo(Basket);
+
+Phone.hasOne(BasketDevice, { foreignKey: 'phoneId', onDelete: 'CASCADE' });
+BasketDevice.belongsTo(Phone);
+
+module.exports = {
+  BasketDevice,
+};
