fba093e776d69cc05c85c9f27e5c39da68efd17a
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-05 15:26:29 +0300

    required new models

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,15 @@
 const { User } = require('./User');
 const { Basket } = require('./Basket');
 const { Token } = require('./Token');
+const { Brand } = require('./Brand');
+const { Phone } = require('./Phone');
+const { BasketDevice } = require('./BasketDevice');
 
 module.exports = {
   User,
   Token,
   Basket,
+  Brand,
+  BasketDevice,
+  Phone,
 };
