fc5062488e9450d3b3c389fd619d90ceb40b3e13
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-06 10:53:53 +0300

    added router with endpoint "/brands"

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 const { userRouter } = require('./userRouter');
 const { authRouter } = require('./authRouter');
+const { brandRouter } = require('./brandRouter');
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
+router.use('/brands', brandRouter);
 router.use('*', (err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
