67d7f5a6159a13647b4394c6431af9c101ef6787
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-06 14:48:23 +0300

    added router "/basketDevice"

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -4,6 +4,7 @@ const { userRouter } = require('./userRouter');
 const { authRouter } = require('./authRouter');
 const { brandRouter } = require('./brandRouter');
 const { phoneRouter } = require('./phoneRouter');
+const { basketDeviceRouter } = require('./basketDeviceRouter');
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use('/users', userRouter);
 router.use('/auth', authRouter);
 router.use('/brands', brandRouter);
 router.use('/phones', phoneRouter);
+router.use('/basketDevice', basketDeviceRouter)
 router.use('*', (err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
