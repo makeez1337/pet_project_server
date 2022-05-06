9152509f8851f1d53cb7b4c2d7a4ff1eb5e89cf7
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-06 11:10:04 +0300

    added router on get to find all brands

--- src/routes/brandRouter.js
+++ src/routes/brandRouter.js
@@ -4,6 +4,7 @@ const { brandController } = require('../controllers/brandController');
 
 const router = Router();
 
+router.get('/', brandController.getBrands)
 router.post('/', brandController.createBrand);
 
 module.exports = {
