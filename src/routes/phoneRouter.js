c5b6b722ee731b650b829052f6f5119aa98c089c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-06 17:51:22 +0300

    created multer to store images on the server, made configs of multer

--- src/routes/phoneRouter.js
+++ src/routes/phoneRouter.js
@@ -1,11 +1,32 @@
 const { Router } = require('express');
+const multer = require('multer');
+
 const { phoneController } = require('../controllers/phoneController');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './src/static/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
 
 const router = Router();
 
 router.get('/', phoneController.getAll);
 router.get('/pagination', phoneController.getPhonesPagination);
-router.post('/', phoneController.createPhone);
+router.post('/', upload.single('phoneImg'), phoneController.createPhone);
 router.delete('/', phoneController.deleteById);
 
 module.exports = {
