b4e33464118e611bf19eac428c7a14d328b16d30
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 01:15:07 +0300

    formatted lines, deleted unused requires

--- src/controllers/ramController.js
+++ src/controllers/ramController.js
@@ -1,12 +1,11 @@
 const { ramServices } = require('../services/ramServices');
-const { memoryController } = require('./memoryController');
-const { memoryService } = require('../services/memoryService');
 
 class RamController {
   async createRam(req, res, next) {
     const { ram } = req.body;
 
     const response = await ramServices.createRam(ram);
+
     res.json(response);
   }
 
