a8fda833c78ecf6204b5674431954135f0842065
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 12:53:33 +0300

    created method to get all memory

--- src/controllers/ramController.js
+++ src/controllers/ramController.js
@@ -1,4 +1,6 @@
 const { ramServices } = require('../services/ramServices');
+const { memoryController } = require('./memoryController');
+const { memoryService } = require('../services/memoryService');
 
 class RamController {
   async createRam(req, res, next) {
@@ -6,6 +8,12 @@ class RamController {
 
     const response = await ramServices.createRam(ram);
     res.json(response);
+  }
+
+  async getAll(req, res, next) {
+    const memory = await memoryService.getAll();
+
+    res.json(memory);
   }
 }
 
