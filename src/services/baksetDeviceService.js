f70f015d54cbd306616a09650e0d931cb96ca4b8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 23:05:42 +0300

    added method getByBasketId

--- src/services/baksetDeviceService.js
+++ src/services/baksetDeviceService.js
@@ -1,8 +1,22 @@
-const { BasketDevice } = require('../models');
+const { BasketDevice, Phone } = require('../models');
+const { sequelize } = require('../db');
 
 class BaksetDeviceService {
   async createBasketDevice(phoneId, basketId) {
     return BasketDevice.create({ phoneId, basketId });
+  }
+
+  async getByBasketId(basketId) {
+    return BasketDevice.findAll({
+      where: { basketId },
+      include: [Phone],
+      attributes: [
+        'phone.id',
+        [sequelize.fn('COUNT', 'phone.id'), 'count'],
+        [sequelize.fn('SUM', sequelize.col('phone.price')), 'totalPrice'],
+      ],
+      group: ['phone.id'],
+    });
   }
 }
 
