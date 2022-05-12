db5d5055744bff1b49b408c59ff22fe4d5069c41
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 18:23:46 +0300

    added endpoint to add item in BasketDevice table

--- src/routes/basketDeviceRouter.js
+++ src/routes/basketDeviceRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+
 const { basketDeviceController } = require('../controllers/basketDeviceController');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 
 const router = Router();
 
 router.post('/', basketDeviceController.createBasketDevice);
+router.post('/addItem', authMiddleware.checkAccessToken, basketDeviceController.addBasketDevice);
 
 module.exports = {
   basketDeviceRouter: router,
