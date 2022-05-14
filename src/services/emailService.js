e2d296537d5b22326060f91e15df7701f448dbb2
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-14 17:49:35 +0300

    email service, created method to send mail

---
+++ src/services/emailService.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+const Email = require('email-templates');
+const path = require('path');
+
+class EmailService {
+  async sendMail(emailToSend, context) {
+    const emailRenderer = new Email({
+      views: {
+        root: path.join(__dirname, '../', 'email-templates'),
+      },
+    });
+
+    context.response.allAmount = context.response.reduce((prev, next) => {
+      return prev + next.totalPrice * Number(next.count);
+    }, 0);
+
+    Object.assign(context, {});
+
+    const html = await emailRenderer.render('productsOrder', context);
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_LOGIN,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: 'Магазин мобільних товарів',
+      to: emailToSend,
+      subject: 'Привіт, твоє замовлення прийнято',
+      html,
+    });
+  }
+}
+
+module.exports = {
+  emailService: new EmailService(),
+};
