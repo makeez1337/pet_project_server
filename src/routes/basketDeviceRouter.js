ef38a9b98b6dc1318c55c2ea4aae8257950186bc
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-15 13:13:35 +0300

    added endpoint "/all" on action delete

--- src/routes/basketDeviceRouter.js
+++ src/routes/basketDeviceRouter.js
@@ -10,6 +10,7 @@ router.post('/', basketDeviceController.createBasketDevice);
 router.post('/addItem', authMiddleware.checkAccessToken, basketDeviceController.addBasketDevice);
 router.post('/confirmPurchase', authMiddleware.checkAccessToken, basketDeviceController.confirmPurchase);
 router.delete('/', authMiddleware.checkAccessToken, basketDeviceController.deleteOneByParams);
+router.delete('/all', basketDeviceController.deleteAllByBasketId)
 
 module.exports = {
   basketDeviceRouter: router,
