78f4bd516411e155d76af186c6b9c3f5929f49ac
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-16 15:38:25 +0300

    added index file

---
+++ src/controllers/index.js
@@ -0,0 +1,17 @@
+const { authController } = require('./authController');
+const { brandController } = require('./brandController');
+const { userController } = require('./userController');
+const { memoryController } = require('./memoryController');
+const { basketDeviceController } = require('./basketDeviceController');
+const { ramController } = require('./ramController');
+const { phoneController } = require('./phoneController');
+
+module.exports = {
+  authController,
+  brandController,
+  userController,
+  memoryController,
+  basketDeviceController,
+  ramController,
+  phoneController,
+};
